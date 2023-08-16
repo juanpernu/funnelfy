d35fd2f851fd2ac1f83c91773f7fd5ffe6259b9d
Juan Manuel Pernumian <juan.pernumian@gmail.com>
2023-08-15 21:40:33 -0300

    feat: create landing home

---
+++ src/components/PrimaryFeatures.jsx
@@ -0,0 +1,149 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import { Tab } from '@headlessui/react'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import backgroundImage from '@/images/background-features.jpg'
+import screenshotExpenses from '@/images/screenshots/expenses.png'
+import screenshotPayroll from '@/images/screenshots/payroll.png'
+import screenshotReporting from '@/images/screenshots/reporting.png'
+import screenshotVatReturns from '@/images/screenshots/vat-returns.png'
+
+const features = [
+  {
+    title: 'Cold Outreach',
+    description:
+      'Instead of sending out those outdated sales decks, stand out from the crowd with a personalized, engaging and eye-catching microsite to get your foot in the door. Book that demo meeting too with our Calendar integrations.',
+    image: screenshotPayroll,
+  },
+  {
+    title: 'Demo/Proposal',
+    description:
+      'Drop in documents, product walkthroughs, dynamic pricing, deal timelines, personalized videos and more. You can even chat back and forth with your prospect within the Pod. Delight your prospects and get deals done quicker.',
+    image: screenshotExpenses,
+  },
+  {
+    title: 'Deal Close',
+    description:
+      'With everything contained in your Pod, simply drop in contracts for signature, onboarding action plans, SLAs and anything else required to get the deal signed, sealed and delivered to Customer Success.',
+    image: screenshotVatReturns,
+  },
+  {
+    title: 'Revenue Intelligence',
+    description:
+      'What live deals have good buyer engagement? Which ones may close? Which ones should you follow up with? Get insights into your whole buyer journey, like never before, with Funnelfy Signals.',
+    image: screenshotReporting,
+  },
+]
+
+export function PrimaryFeatures() {
+  let [tabOrientation, setTabOrientation] = useState('horizontal')
+
+  useEffect(() => {
+    let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
+
+    function onMediaQueryChange({ matches }) {
+      setTabOrientation(matches ? 'vertical' : 'horizontal')
+    }
+
+    onMediaQueryChange(lgMediaQuery)
+    lgMediaQuery.addEventListener('change', onMediaQueryChange)
+
+    return () => {
+      lgMediaQuery.removeEventListener('change', onMediaQueryChange)
+    }
+  }, [])
+
+  return (
+    <section
+      id="features"
+      aria-label="Features for running your books"
+      className="relative overflow-hidden bg-violet-600 pb-28 pt-20 sm:py-32"
+    >
+      <Container className="relative">
+        <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
+          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
+            Stand out in today’s saturated B2B sales industry.
+          </h2>
+          <p className="mt-6 text-lg tracking-tight text-blue-100">
+            Create an asynchronous communication environment that simplifies
+            your buyer's journey, making decisions easier.
+          </p>
+        </div>
+        <Tab.Group
+          as="div"
+          className="mt-16 grid grid-cols-1 items-center gap-y-2 pt-10 sm:gap-y-6 md:mt-20 lg:grid-cols-12 lg:pt-0"
+          vertical={tabOrientation === 'vertical'}
+        >
+          {({ selectedIndex }) => (
+            <>
+              <div className="-mx-4 flex overflow-x-auto pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
+                <Tab.List className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
+                  {features.map((feature, featureIndex) => (
+                    <div
+                      key={feature.title}
+                      className={clsx(
+                        'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6',
+                        selectedIndex === featureIndex
+                          ? 'bg-white lg:bg-white/10 lg:ring-1 lg:ring-inset lg:ring-white/10'
+                          : 'hover:bg-white/10 lg:hover:bg-white/5'
+                      )}
+                    >
+                      <h3>
+                        <Tab
+                          className={clsx(
+                            'font-display text-lg ui-not-focus-visible:outline-none',
+                            selectedIndex === featureIndex
+                              ? 'text-violet-600 lg:text-white'
+                              : 'text-violet-100 hover:text-white lg:text-white'
+                          )}
+                        >
+                          <span className="absolute inset-0 rounded-full lg:rounded-l-xl lg:rounded-r-none" />
+                          {feature.title}
+                        </Tab>
+                      </h3>
+                      <p
+                        className={clsx(
+                          'mt-2 hidden text-sm lg:block',
+                          selectedIndex === featureIndex
+                            ? 'text-white'
+                            : 'text-violet-100 group-hover:text-white'
+                        )}
+                      >
+                        {feature.description}
+                      </p>
+                    </div>
+                  ))}
+                </Tab.List>
+              </div>
+              <Tab.Panels className="lg:col-span-7">
+                {features.map((feature) => (
+                  <Tab.Panel key={feature.title} unmount={false}>
+                    <div className="relative sm:px-6 lg:hidden">
+                      <div className="absolute -inset-x-4 bottom-[-4.25rem] top-[-6.5rem] bg-white/10 ring-1 ring-inset ring-white/10 sm:inset-x-0 sm:rounded-t-xl" />
+                      <p className="relative mx-auto max-w-2xl text-base text-white sm:text-center">
+                        {feature.description}
+                      </p>
+                    </div>
+                    <div className="mt-10 w-[45rem] overflow-hidden rounded-xl bg-slate-50 shadow-xl shadow-violet-900/20 sm:w-auto lg:mt-0 lg:w-[67.8125rem]">
+                      <Image
+                        className="w-full"
+                        src={feature.image}
+                        alt=""
+                        priority
+                        sizes="(min-width: 1024px) 67.8125rem, (min-width: 640px) 100vw, 45rem"
+                      />
+                    </div>
+                  </Tab.Panel>
+                ))}
+              </Tab.Panels>
+            </>
+          )}
+        </Tab.Group>
+      </Container>
+    </section>
+  )
+}
